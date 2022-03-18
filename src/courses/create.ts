8444746c4f7fa6f7265b43893ebfd11805254959
Rubens Henrique Pereira <rubensh206@gmail.com>
2022-03-18 14:13:11 -0300

    ManyToMany

--- src/courses/create.ts
+++ src/courses/create.ts
@@ -5,22 +5,13 @@ const prisma = new PrismaClient();
 async function main() {
   const result = await prisma.courses.create({
     data: {
-      name: "Curso de Java",
-      duration: 500,
-      description: "Curso de Java 17",
-      teacher: {
-        connectOrCreate: {
-          where: {
-            name: "Daniele Leão Evangelista",
-          },
-          create: {
-            name: "Daniele Leão Evangelista",
-          },
-        },
-      },
+      duration: 300,
+      name: "Curso de React Native",
+      description: "Curso excelente de React Native com Rodrigão",
     },
   });
 
   console.log(result);
 }
-main()
+
+main();
