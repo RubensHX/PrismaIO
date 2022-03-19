191b82ae2f00d8627c205e0d1495b3e8ac19cc61
Rubens Henrique Pereira <rubensh206@gmail.com>
2022-03-19 13:08:04 -0300

    Create paginator.ts

---
+++ src/pagination/paginator.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+
+    let skip = 0
+
+    const result = await prisma.courses.findMany({
+        skip: skip,
+        take: 3,
+    })
+}
+main()
